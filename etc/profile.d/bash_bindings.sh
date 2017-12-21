32d0b7cbe7636c08388a631de71ef27a0a20f85b
Facundo Victor <facundovt@gmail.com>
2017-12-21 15:56:44 -0300

    Fix shelcheck

--- etc/profile.d/bash_bindings.sh
+++ etc/profile.d/bash_bindings.sh
@@ -9,6 +9,10 @@ bind '"\C-v":"vim \n"';
 # Bind delete from cursor to end of word
 bind '"\C-f":"\ed"';
 
+# Bind git stash-pick to C-s
+#shellcheck disable=SC2016
 bind '"\C-s":"$(git stash-pick)\n"';
 
+# Bind git modified-file-pick to C-q
+#shellcheck disable=SC2016
 bind '"\C-q":"$(git modified-file-pick)\n"';
