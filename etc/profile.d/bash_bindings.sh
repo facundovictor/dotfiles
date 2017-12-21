7c9fc54863e77f25072a52a239a980c125971634
Facundo Victor <facundovt@gmail.com>
2017-12-21 15:56:10 -0300

    Add bind C-f to delete from cursor to end of word

--- etc/profile.d/bash_bindings.sh
+++ etc/profile.d/bash_bindings.sh
@@ -6,7 +6,9 @@ stty -ixon;
 # Bind open vim to Ctrl-V
 bind '"\C-v":"vim \n"';
 
-# Bind open vim to Ctrl-V
+# Bind delete from cursor to end of word
+bind '"\C-f":"\ed"';
+
 bind '"\C-s":"$(git stash-pick)\n"';
 
 bind '"\C-q":"$(git modified-file-pick)\n"';
