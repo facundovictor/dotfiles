2aaec8ee795abacac43edb0b2ce3dad0e5a88183
Facundo Victor <facundovt@gmail.com>
2018-02-19 18:05:58 -0300

    Re-indent function to not exceed 80 line length

--- etc/profile.d/getcert.sh
+++ etc/profile.d/getcert.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
 # Print out the details of the fqdn certificate
+# Similar to what 'nmap -p 443 --script ssl-cert' but faster and without
+# having to install nmap.
 function getcert () {
-	local fqdn="${1:-}"
-	echo | \
-		openssl s_client -showcerts -servername "$fqdn" -connect "$fqdn":443 2>/dev/null | \
-		openssl x509 -inform pem -noout -text
+    local fqdn="${1:-}"
+    echo | \
+        openssl s_client -showcerts \
+                         -servername "$fqdn" \
+                         -connect "$fqdn":443 \
+                         2>/dev/null | \
+        openssl x509 -inform pem -noout -text
 }
