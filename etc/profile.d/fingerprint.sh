126dcced731873632bc8221b3ecd1bfbbb06856e
Facundo Victor <facundovt@gmail.com>
2019-07-18 20:56:23 -0300

    Add quick crypto helpers

---
+++ etc/profile.d/fingerprint.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Reference:
+# 	- https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-key-pairs.html#verify-key-pair-fingerprints
+
+function awsKeyFingerprint () {
+    local key_path="${1:-}"
+    openssl pkcs8 -in "$key_path" -inform PEM -outform DER -topk8 -nocrypt | openssl sha1 -c
+}
+
+function awsKeyFingerprint-third-party () {
+    local key_path="${1:-}"
+    openssl rsa -in "$key_path" -pubout -outform DER | openssl md5 -c
+}
+
+function public_pem_to_ssh_rsa_format () {
+    local key_path="${1:-}"
+    ssh-keygen -f "$key_path" -i -mPKCS8
+}
